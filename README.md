8114c9b68a67947c2c1ad945691be37a1cb700fd
Adrian <g.adrian.gonzalez.90@gmail.com>
2026-04-14 22:26:15 -0600

    Initialize project with AbstractStone model and Lombok configuration

---
+++ README.md
@@ -0,0 +1,7 @@
+# Spring Boot
+
+## POO Fundamentals
+
+### Herencia
+
+Es un mecanismo mediante el cual una clase (denominada clase hija o subclase) hereda atributos y métodos de otra clase (llamada clase padre o superclase). Esto permite reutilizar código, facilitando la creación de nuevas clases que extienden la funcionalidad de las existentes sin tener que reescribirlas.
--- poo-fundamentals/src/main/java/com/driagon/models/AbstractStone.java
+++ poo-fundamentals/src/main/java/com/driagon/models/AbstractStone.java
@@ -1,4 +1,27 @@
 package com.driagon.models;
 
-public class AbstractStone {
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+public abstract class AbstractStone {
+
+    String color;
+    String name;
+    String location;
+    Integer energyLevel;
+    Integer numberOfSides;
+
+    public AbstractStone(String color, String name, String location, Integer energyLevel, Integer numberOfSides) {
+        this.color = color;
+        this.name = name;
+        this.location = location;
+        this.energyLevel = energyLevel;
+        this.numberOfSides = numberOfSides;
+    }
 }
